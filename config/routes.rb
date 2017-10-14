9094b73b3e2c45e9a5c90a577729b96e09c6873f
Noah Wimmer <nwimmer123@yahoo.com>
2017-10-14 16:40:16 -0700

    basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "users#index"
+  
+    resources :users
+      
+    get "/login", to: "sessions#new", as: "login"
+    get "/logout", to: "sessions#destroy"
+    post "/login", to: "sessions#create"
+    
 end
