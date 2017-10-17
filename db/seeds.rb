3095f9f1015b908962adee22743115fb295b92f5
Noah Wimmer <nwimmer123@yahoo.com>
2017-10-17 16:46:02 -0700

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,16 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create({ name: 'Star Wars' , { name: 'Lord of the Rings' )
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name:"Joe", email:"a@com", password: "a")
+user2 = User.create( name: "Mary", email: "b@com" , password: "s"  )
+user3 = User.create( name: "Jack", email: "c@com" , password:  "d" )
+user4 = User.create( name: "Breon", email: "d@com" , password:  "f" )
+user5 = User.create( name: "Shawnique", email: "e@com" , password: "g"  )
+user6 = User.create( name: "Danny", email: "f@com" , password: "h"  )
+user7 = User.create( name: "Reba", email: "g@com" , password: "j"  )
+user8 = User.create( name: "Kehontas", email: "h@com" , password:  "k" )
+user9 = User.create( name: "Rachel", email: "i@com" , password:  "l" )
+user10 = User.create( name: "Eloise", email: "j@com" , password:  "m" )
